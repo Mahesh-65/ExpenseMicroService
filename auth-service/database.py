23ea3ccd1eddce789b0e4de847b9e256cd96cf12
Mahesh D <104552634+Mahesh-65@users.noreply.github.com>
2026-02-25 18:02:15 +0530

    Update database.py

--- auth-service/database.py
+++ auth-service/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://admin:<admin123>@database-1.cfwqy0kysytm.ap-south-1.rds.amazonaws.com:3306/expense_service_db"
+DATABASE_URL = "mysql+pymysql://admin:admin123@database-1.cfwqy0kysytm.ap-south-1.rds.amazonaws.com:3306/expense_service_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
