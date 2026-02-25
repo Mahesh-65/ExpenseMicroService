499292e80e01bbc7e4fbb62fa7dea0ba7a5d0bbb
Mahesh D <104552634+Mahesh-65@users.noreply.github.com>
2026-02-25 16:25:16 +0530

    Update database.py

--- expense-service/database.py
+++ expense-service/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:root@db/expense_db"
+DATABASE_URL = "mysql+pymysql://admin:<admin123>@database-1.cfwqy0kysytm.ap-south-1.rds.amazonaws.com:3306/expense_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine)
